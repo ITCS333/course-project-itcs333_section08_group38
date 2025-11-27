a6713a3cd828b1f4b36bd62bb97f1da0609fa265
s-debug0 <sara0398h@gmail.com>
2025-11-27 23:48:15 +0300

    done login js

--- src/auth/login.js
+++ src/auth/login.js
@@ -18,12 +18,17 @@
 // the HTML document is parsed before this script runs.
 
 // TODO: Select the login form. (You'll need to add id="login-form" to the <form> in your HTML).
+const loginForm=document.getElementById("login-form");
+
 
 // TODO: Select the email input element by its ID.
+const emailInput=document.getElementById("email");
 
 // TODO: Select the password input element by its ID.
+const passInput=document.getElementById("password");
 
 // TODO: Select the message container element by its ID.
+const messageContainer=document.getElementById("message-container");
 
 // --- Functions ---
 
@@ -40,6 +45,8 @@
  */
 function displayMessage(message, type) {
   // ... your implementation here ...
+  messageContainer.textContent=message;
+  messageContainer.classList.add("type");
 }
 
 /**
@@ -56,6 +63,9 @@ function displayMessage(message, type) {
  */
 function isValidEmail(email) {
   // ... your implementation here ...
+
+  regex= /^[a-z]+@[a-z]+\.[a-z]+$/i
+  return regex.test(email);
 }
 
 /**
@@ -70,6 +80,7 @@ function isValidEmail(email) {
  */
 function isValidPassword(password) {
   // ... your implementation here ...
+  return password.length>=8
 }
 
 /**
@@ -88,6 +99,23 @@ function isValidPassword(password) {
  */
 function handleLogin(event) {
   // ... your implementation here ...
+  event.preventDefault();
+
+  email=emailInput.value.trim();
+  pass=passInput.value.trim();
+
+  if(!isValidEmail(email)){
+    displayMessage("Invalid email format.", "error")
+  }
+  else if (!isValidPassword(pass)){
+    displayMessage("Password must be at least 8 characters.", "error");
+  }
+  else{
+    displayMessage("Login successful!", "success")
+  }
+
+  emailInput="";
+  passInput="";
 }
 
 /**
@@ -100,6 +128,10 @@ function handleLogin(event) {
  */
 function setupLoginForm() {
   // ... your implementation here ...
+  if(loginForm)
+  {
+    loginForm.addEventListener("submit",handleLogin);
+  }
 }
 
 // --- Initial Page Load ---
