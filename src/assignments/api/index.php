ad6a78459798ab03e008f90d1e938006dfa68b96
Rehan Saleh Hassan <reekh6677@gmail.com>
2025-12-01 23:20:40 +0300

    Update index.php

--- src/assignments/api/index.php
+++ src/assignments/api/index.php
@@ -45,7 +45,6 @@ header('Content-Type: application/json; charset=utf-8');
 
 
 // TODO: Set CORS headers to allow cross-origin requests
-header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
